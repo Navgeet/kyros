ab53e35dc35fce309ab55b2496b697f655ef0113
Navgeet Agrawal <navgeet.agrawal@gmail.com>
2025-07-24 02:40:48 +1000

    add missing utils file

---
+++ utils.py
@@ -0,0 +1,18 @@
+import asyncio
+from prompt_toolkit import PromptSession
+from prompt_toolkit.styles import Style
+from prompt_toolkit.history import FileHistory
+import os
+
+history_file = os.path.expanduser("~/.triton_history")
+session = PromptSession(history=FileHistory(history_file))
+
+async def input():
+    """Get input from the user with history support, ensuring it is not empty."""
+    style = Style.from_dict({
+        "prompt": "ansigreen bold",
+    })
+    msg = ""
+    while msg.strip() == "":  # don't send empty input
+        msg = await session.prompt_async([("class:prompt", "> ")], style=style, multiline=False, complete_while_typing=False, enable_system_prompt=False)
+    return msg
